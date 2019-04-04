118306e419415587e514bfef866c54aca15e58e5
Servidor <SERVIDOR@DESKTOP-G4A15LL>
2019-04-04 04:32:13 -0300

    PBL2

---
+++ VendaMoveis/VendaMoveis/Models/Funcionario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace VendaMoveis.Models
+{
+    public enum StatusFuncionario { Disponivel, Indisponivel }
+    public class Funcionario
+    {
+        [Key]
+        public int Pk_Funcionario { get; set; }
+        public string Nome { get; set; }
+        public StatusFuncionario StatusFuncionario { get; set; }
+
+
+
+
+        [ForeignKey("Movel")]
+        public int Fk_Movel { get; set; }
+        public Movel Movel { get; set; }
+
+
+
+
+        public void atribuiStatusFuncionario()
+        {
+            if (Movel.StatusMovel == StatusMovel.EmConstrução)
+            {
+                StatusFuncionario = StatusFuncionario.Indisponivel;
+            }
+            else
+            {
+                StatusFuncionario = StatusFuncionario.Disponivel;
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ VendaMoveis/VendaMoveis/Models/Movel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VendaMoveis.Models
+{
+    public enum StatusMovel { Solicitado, EmConstrução, Entregue }
+    public class Movel
+    {
+        [Key]
+        public int Pk_Movel { get; set; }
+        public string Nome { get; set; }
+        public string Cor { get; set; }
+        public string Medidas { get; set; }
+        public string Material { get; set; }
+        public string Link { get; set; }
+        public double Valor { get; set; }
+        public StatusMovel StatusMovel { get; set; }
+        public virtual ICollection<Funcionario> Funcionarios { get; set; }
+
+
+
+        public Movel()
+        {
+            StatusMovel = StatusMovel.Solicitado;
+        }
+
+
+
+
+    }
+}
